9bc1d16af4cf35465e79ec727d334870bf1c79b4
NewwwChoppa <zapevalov-2004@mail.ru>
2023-11-14 13:51:42 +0300

    initial commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Андрей on 14.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
