288acbacc646a51ccf09e4bb5f382769876951f7
NewwwChoppa <zapevalov-2004@mail.ru>
2023-11-16 11:54:45 +0300

    Data Model

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -9,44 +9,47 @@ import UIKit
 
 class MainViewController: UITableViewController {
     
-    let restaurantNames = ["Бургер Кинг", "Вкусно и точка", "Совок", "Еица на углях", "Ростикс"]
-
+    let places = [Place(name: "Бургер Кинг", location: "Нижний Новгород", type: "Фастфуд", image: "Бургер Кинг"),
+                  Place(name: "Вкусно и точка", location: "Нижний Новгород", type: "Фастфуд", image: "Вкусно и точка"),
+                  Place(name: "Совок", location: "Нижний Новгород", type: "Лапшичная", image: "Совок"),
+                  Place(name: "Еица на углях", location: "Нижний Новгород", type: "Шаурма", image: "Еица на углях"),
+                  Place(name: "Ростикс", location: "Нижний Новгород", type: "Фастфуд", image: "Ростикс")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return restaurantNames.count
+        return places.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         
-        cell.nameLabel.text = "\(restaurantNames[indexPath.row])"
-        cell.imageOfPlace.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.nameLabel.text = "\(places[indexPath.row].name)"
+        cell.locationLabel.text = "\(places[indexPath.row].location)"
+        cell.typeLabel.text = "\(places[indexPath.row].type)"
+        cell.imageOfPlace.image = UIImage(named: places[indexPath.row].image)
+        
         cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.height / 2
         cell.imageOfPlace.clipsToBounds = true
         
         return cell
     }
     
-    // MARK: - TableView Delegate
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 85
-    }
-
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Андрей on 16.11.2023.
+//
+
+import Foundation
+
+struct Place {
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+}
