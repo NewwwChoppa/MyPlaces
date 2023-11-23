6b943d37cb8e117c79ab52639246a0bccf4222e0
NewwwChoppa <zapevalov-2004@mail.ru>
2023-11-23 14:48:14 +0300

    tableVC replaced by viewController

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 import RealmSwift
 
-class MainViewController: UITableViewController {
+class MainViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
     
     var places: Results<Place>!
     
@@ -18,54 +20,54 @@ class MainViewController: UITableViewController {
         places = realm.objects(Place.self)
     }
     
-//     MARK: - Table view data source
+    //     MARK: - Table view data source
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return places.isEmpty ? 0 : places.count
     }
     
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-                let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         
-                let place = places[indexPath.row]
+        let place = places[indexPath.row]
         
-                cell.nameLabel.text = place.name
-                cell.locationLabel.text = place.location
-                cell.typeLabel.text = place.type
+        cell.nameLabel.text = place.name
+        cell.locationLabel.text = place.location
+        cell.typeLabel.text = place.type
         
-                var imageData = Data()
+        var imageData = Data()
         
-                if place.imageData != nil {
-                    imageData = place.imageData!
-                } else {
-                    imageData = UIImage(named: "photo.on.ractangle")!.pngData()!
-                }
+        if place.imageData != nil {
+            imageData = place.imageData!
+        } else {
+            imageData = UIImage(named: "photo.on.ractangle")!.pngData()!
+        }
         
-                cell.imageOfPlace.image = UIImage(data: imageData)
+        cell.imageOfPlace.image = UIImage(data: imageData)
         
-                cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.height / 2
-                cell.imageOfPlace.clipsToBounds = true
-                cell.imageOfPlace.contentMode = .scaleAspectFill
+        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.height / 2
+        cell.imageOfPlace.clipsToBounds = true
+        cell.imageOfPlace.contentMode = .scaleAspectFill
         
-                return cell
+        return cell
     }
     
     
-// Альтернативный метод для добавления нескольких действий по свайпу
+    // Альтернативный метод для добавления нескольких действий по свайпу
     
-//    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-//        
-//        let place = places[indexPath.row]
-//        
-//        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { _, _, _ in
-//            StorageManager.deleteObject(place)
-//            tableView.deleteRows(at: [indexPath], with: .automatic)
-//        }
-//        return UISwipeActionsConfiguration(actions: [deleteAction])
-//    }
+    //    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+    //
+    //        let place = places[indexPath.row]
+    //
+    //        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { _, _, _ in
+    //            StorageManager.deleteObject(place)
+    //            tableView.deleteRows(at: [indexPath], with: .automatic)
+    //        }
+    //        return UISwipeActionsConfiguration(actions: [deleteAction])
+    //    }
     
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
         if editingStyle == .delete {
             let place = places[indexPath.row]
@@ -75,23 +77,23 @@ class MainViewController: UITableViewController {
         }
         
     }
-        
-
-         // MARK: - Navigation
-         
-         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-             if segue.identifier == "showDetail" {
-                 guard let indexPath = tableView.indexPathForSelectedRow else { return }
-                 let place = places[indexPath.row]
-                 let newPlaceVC = segue.destination as! NewPlaceViewController
-                 newPlaceVC.currentPlace = place
-             }
-         }
-        
-        @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
-            guard let newPlaceVC = segue.source as? NewPlaceViewController else { return }
-            newPlaceVC.savePlace()
-            tableView.reloadData()
+    
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            guard let indexPath = tableView.indexPathForSelectedRow else { return }
+            let place = places[indexPath.row]
+            let newPlaceVC = segue.destination as! NewPlaceViewController
+            newPlaceVC.currentPlace = place
         }
-        
     }
+    
+    @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
+        guard let newPlaceVC = segue.source as? NewPlaceViewController else { return }
+        newPlaceVC.savePlace()
+        tableView.reloadData()
+    }
+    
+}
